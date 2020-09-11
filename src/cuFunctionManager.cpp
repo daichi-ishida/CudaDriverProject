2aa54a6f9e4480cd83d6ecff1e38d0c18a9a0efa
Daichi Ishida <daichi.phys.lab@gmail.com>
2020-09-12 04:54:02 +0900

    add ptxJIT

--- src/cuFunctionManager.cpp
+++ src/cuFunctionManager.cpp
@@ -1,6 +1,7 @@
 #include "cuFunctionManager.h"
 
 #include <cassert>
+#include <fstream>
 
 CuFunctionsManager* CuFunctionsManager::instance_ = nullptr;
 CUcontext CuFunctionsManager::cu_context = NULL;
@@ -39,11 +40,73 @@ void CuFunctionsManager::destroy()
 
 void CuFunctionsManager::loadModule(const std::string& name)
 {
-    CUmodule cu_module;
+    CUjit_option options[6];
+    void *optionVals[6];
+    char error_log[8192], info_log[8192];
+    unsigned int logSize = 8192;
+    void *cuOut;
+    size_t outSize;
+    std::string ptx_source;
 
+    // Setup linker options
+    // Pass a buffer for info messages
+    options[0] = CU_JIT_INFO_LOG_BUFFER;
+    optionVals[0] = (void *)info_log;
+    // Pass the size of the info buffer
+    options[1] = CU_JIT_INFO_LOG_BUFFER_SIZE_BYTES;
+    optionVals[1] = (void *)(long)logSize;
+    // Pass a buffer for error message
+    options[2] = CU_JIT_ERROR_LOG_BUFFER;
+    optionVals[2] = (void *)error_log;
+    // Pass the size of the error buffer
+    options[3] = CU_JIT_ERROR_LOG_BUFFER_SIZE_BYTES;
+    optionVals[3] = (void *)(long)logSize;
+    // Make the linker verbose
+    options[4] = CU_JIT_LOG_VERBOSE;
+    optionVals[4] = (void *)1;
+
+    CUlinkState lState;
+
+    // Create a pending linker invocation
+    cuLinkCreate(5, options, optionVals, &lState);
+
+    // load ptx
     std::string filename = name + ".ptx";
-    CUresult res = cuModuleLoad(&cu_module, filename.c_str());
+    std::ifstream ifs(filename, std::ios::binary);
+    ifs.seekg(0, std::ios::end);
+    int file_size = ifs.tellg();
+    ifs.seekg(0);
+
+    char *buf = new char[file_size + 1];
+    ifs.read(buf, file_size);
+
+    buf[file_size] = '\0';
+    ptx_source = buf;
+    delete[] buf;
+
+    // Load the PTX from the ptx file
+    printf("Loading %s kernel program\n", filename.c_str());
+    CUresult res = cuLinkAddData(lState, CU_JIT_INPUT_PTX, (void *)ptx_source.c_str(),
+                            strlen(ptx_source.c_str()) + 1, 0, 0, 0, 0);
+
+    if (res != CUDA_SUCCESS) {
+        // Errors will be put in error_log, per CU_JIT_ERROR_LOG_BUFFER option
+        // above.
+        fprintf(stderr, "PTX Linker Error:\n%s\n", error_log);
+    }
+
+    // Complete the linker step
+    cuLinkComplete(lState, &cuOut, &outSize);
+
+    // info_log were requested in options above.
+    printf("%s\n", info_log);
+
+    CUmodule cu_module;
+    res = cuModuleLoadData(&cu_module, cuOut);
     assert(res == CUDA_SUCCESS && "Can't load module");
+    
+    // Destroy the linker invocation
+    cuLinkDestroy(lState);
 
     modules_[name] = cu_module;
 }
